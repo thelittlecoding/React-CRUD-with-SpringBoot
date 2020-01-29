e18e03f0c1d807a7c382e45cf5565c2f5425bc56
Jake <jakeshin89@naver.com>
2020-01-30 01:31:24 +0900

    1st Commit

---
+++ front-end/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import AppRouter from './component/route/RouterComponent';
+import NavBar from "./component/route/NavBar";
+
+import Container from '@material-ui/core/Container';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Container>
+        <AppRouter />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ front-end/src/component/basic/Function.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserComponent = (props) => {
+  return(
+    <div className="container">
+      컴포넌트 내용 작성부분
+    </div>
+  );
+}
+
+export default UserComponent;
